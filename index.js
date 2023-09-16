d742d1727ab696560f516f28bbd08bc74f749278
Lyra Freund <lyrafreund@gmail.com>
2023-09-16 11:51:46 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// function saturdayFun(activity) {
+//     if (activity = activity) {
+//         return "This Saturday, I want to " + activity + "!"
+//     } else {
+//         return "This Saturday, I want to roller-skate!"
+//     }
+// }
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+// console.log(saturdayFun())
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(char){
+    return function(character = "special"){
+        return `You are ${char}${character}${char}!`
+    }
+}
+
+console.log(wrapAdjective("*")("a dedicated programmer"));
